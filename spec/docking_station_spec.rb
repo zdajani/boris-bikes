30ce9c2207c391e63c28110a5011af604e596d56
Zeina Dajani <dajani.zeina@gmail.com>
2015-06-10 16:46:51 +0100

    added release_bike functionality

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,11 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it 'release working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
 
 #station = DockingStation.new
