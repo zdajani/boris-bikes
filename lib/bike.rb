ba609c3c479d030ece78e22ddbc363de89a9628f
Natalie Akam <natalie.akam@gmail.com>
2015-06-11 18:08:20 +0100

    Created test and method to check if bikes are working

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,19 @@
 class Bike
-  def working?
-    true
+  
+  def initialize
+    @working = true
   end
+
+  def working?
+    @working
+   end 
+  
+  def report_broken
+    @working = false
+  end
+  
+ def broken?
+   !@working
+  end
+
 end
