56bf38a5f1d2c6595dfad48cce210844068d85df
Zeina Dajani <dajani.zeina@gmail.com>
2015-06-12 12:02:08 +0100

    added doubles and refactored DockingStation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,6 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
-  xit "knows when it doesn't have any bikes" do
-    expect(subject.empty?).to be true
-  end
 
 
   describe 'release_bike' do
@@ -14,29 +11,28 @@ describe DockingStation do
     end
 
     it 'release working bikes' do
-      subject.dock Bike.new
-      bike = subject.release_bike
-      expect(bike).to be_working
+      bike = double :bike, working?: true
+      subject.dock bike
+      expect(subject.release_bike).to be bike
+    end
+
+    it 'does not release broken bikes' do
+      bike = double :bike, working?: false
+      subject.dock bike
+      expect {subject.release_bike}.to raise_error "No bikes available"
     end
   end
 
 
   describe 'dock' do
   it 'raises an error when full' do
-    subject.capacity.times { subject.dock Bike.new }
-    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    subject.capacity.times { subject.dock double :bike }
+    expect { subject.dock double :bike }.to raise_error 'Docking station full'
     end
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  xdescribe 'docking a bike' do
-    it "docks a bike" do
-      bike = Bike.new
-      subject.dock bike
-      expect(subject.empty?).to be
-    end
-  end
 
 
   it 'has a default capacity' do
@@ -48,6 +44,7 @@ describe DockingStation do
     subject.capacity = 50
     expect(subject.capacity).to eq 50
   end
+
 
 
 end
